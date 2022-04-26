b75e76b8402abb6dc6b2714667b83a7bce73705e
Dmitry Kotenko <d.kotenko@yandex.ru>
2022-04-27 00:02:08 +0300

    Swift package metadata

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftTryCatch",
+    products: [
+        .library(
+            name: "SwiftTryCatch",
+            targets: ["SwiftTryCatch"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftTryCatch", path: "Sources"
+        )
+    ]
+)
